d3dbce3fbdb535c2e099224517f65109217e7d2b
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-03-15 23:30:01 +0500

    Dependencycheck integration

---
+++ models/integration_pd.py
@@ -0,0 +1,18 @@
+from typing import Optional, List
+from pylon.core.tools import log
+from pydantic import BaseModel, validator
+
+
+class IntegrationModel(BaseModel):
+    comp_path: Optional[str] = '/tmp/code'
+    comp_opts: Optional[str] = ""
+    save_intermediates_to: Optional[str] = '/data/intermediates/sast'
+
+    def check_connection(self) -> bool:
+        try:
+            return True
+        except Exception as e:
+            log.exception(e)
+            return False
+    
+
